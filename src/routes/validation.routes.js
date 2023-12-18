c43f90879dbb773f088ed3fd82ff12045ce235cc
cconfort <113508854+csconfort1@users.noreply.github.com>
2023-12-18 15:28:41 +0000

    Delete src directory

--- src/routes/validation.routes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-
-const ValidationController = require("../controllers/ValidationController");
-const validationController = new ValidationController();
-
-const validationRoutes = Router();
-validationRoutes.post("/", validationController.create);
-
-module.exports = validationRoutes;
